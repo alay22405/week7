156a14c6a693e30f44b9223a4d8e1baa0bef8658
Halay395 <66864762+Halay395@users.noreply.github.com>
2021-02-19 18:37:31 -0800

    adding first file

---
+++ SeactionA.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+
+class URL {
+private:
+
+
+    std::string scheme;
+    std::string authority;
+    std::string path;
+
+public:
+    URL(std::string url) {
+        int first = url.find(":");
+        scheme = url.substr(0, first);
+        std::string restUrl = url.substr(first + 3, url.length());
+        int thirdIndex = restUrl.find("/");
+        authority = url.substr(first + 3, thirdIndex);
+        path = restUrl.substr(thirdIndex, url.length());
+
+    }
+    std::string getScheme() {
+        return scheme;
+    }
+    std::string getAuthority() {
+        return authority;
+    }
+    std::string getPath() {
+        return path;
+    }
+    std::string getFullURL() {
+        return scheme + "://" + authority + path;
+    }
+};
